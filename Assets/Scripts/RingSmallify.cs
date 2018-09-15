2506b63671eb4841787f023697ee4257421c4b0b
Chamuth Chamandana <c.chamandana@gmail.com>
2018-09-16 01:04:13 +0530

    design for powerups

---
+++ Assets/Scripts/RingSmallify.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingSmallify : MonoBehaviour {
+
+    void Update()
+    {
+        if (Global.Playable)
+        {
+            // in 120 seconds to 0.5
+            transform.localScale = new Vector3(transform.localScale.x - (Time.deltaTime / 600), transform.localScale.x - (Time.deltaTime / 600));
+        }
+    }
+}
